7060b83dae2fdec336b6c51f921d97c42e7bd351
shinji fujita <fujita.shinji@jp.fujitsu.com>
2015-10-09 15:36:53 +0900

    change from fj directory to personium directory.

--- core/src/main/java/com/fujitsu/dc/core/DcCoreConfig.java
+++ core/src/main/java/com/fujitsu/dc/core/DcCoreConfig.java
@@ -620,7 +620,7 @@ public class DcCoreConfig {
          * RepairAdsを起動するか否かを示すファイルのパス. ファイルが存在する場合のみ、RepairAdsが起動する。
          */
         public static final String REPAIR_ADS_INVOCATION_FILE_PATH = KEY_ROOT + "es.ads.repair.invocationFlagFile";
-        static final String DEFAULT_REPAIR_ADS_INVOCATION_FILE_PATH = "/fj/dc-core/invokeRepair";
+        static final String DEFAULT_REPAIR_ADS_INVOCATION_FILE_PATH = "/personium/dc-core/invokeRepair";
     }
 
     /**
